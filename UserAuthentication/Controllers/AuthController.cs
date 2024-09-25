807cc6850405aaf2729514c4900499e73c0ca051
c-inodeo <c.inodeo@arcanys.com>
2024-09-25 18:13:37 +0800

    Update 25/09 - Added the initial code of API controller that will hold the endpoints for user login and registration

---
+++ UserAuthentication/Controllers/AuthController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.V4.Pages.Account.Internal;
+using Microsoft.AspNetCore.Mvc;
+using ECommerceWebMicroservices.Models;
+
+namespace UserAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AuthController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] Register model)
+        {
+            var user = new IdentityUser { UserName = model.Username, Email = model.EmailAddress };
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded) 
+            {
+                return Ok(new { message = "User registered successfully" });
+            }
+            return BadRequest();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] Login login)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await _signInManager.PasswordSignInAsync(login.Email, login.Password, login.RememberMe, false);
+            if (result.Succeeded)
+            {
+                return Ok(new { message = "Login successful" });
+            }
+            return Unauthorized(new { message = "Invalid username or password" });
+        }
+    }
+}
